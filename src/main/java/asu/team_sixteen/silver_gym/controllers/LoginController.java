c391040f7cf8eb225d8f2dbd42049e69b93fcb61
A.Ramy <ahmedramysaidelnaggar@gmail.com>
2025-04-17 22:32:25 +0200

    Login screen

--- src/main/java/asu/team_sixteen/silver_gym/controllers/LoginController.java
+++ src/main/java/asu/team_sixteen/silver_gym/controllers/LoginController.java
@@ -9,26 +9,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
-    @FXML
-    Button loginButton;
-    @FXML
-    Button signupButton;
-
-    @FXML
-    protected void onLoginButtonClick() {
-
-    }
-    @FXML
-    protected void onSignupButtonClick() {
-
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loginButton.prefWidthProperty().bind(Main.primaryStage.widthProperty().divide(5));
-        loginButton.prefHeightProperty().bind(Main.primaryStage.heightProperty().divide(10));
 
-        signupButton.prefWidthProperty().bind(Main.primaryStage.widthProperty().divide(5));
-        signupButton.prefHeightProperty().bind(Main.primaryStage.heightProperty().divide(10));
     }
 }
